af4cf1a577ec94e4b3a40229e5035e4d2b15a6b3
denfushi <awadomar@gmail.com>
2024-10-09 09:39:36 -0400

    icons instead of images and redo of title logo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import "./App.css";
 import googlePlayImage from "./assets/playstore.png";
 import appStoreImage from "./assets/appstore.png";
-import qrCodeImage from "./assets/LOMAeventsQR.svg";
 import logoImage from "./assets/logo.png";
 import medialabLogo from "./assets/logo.png";
-import usp1 from "./assets/toa-heftiba-TVQFl9R-MLQ-unsplash.jpg";
-import usp2 from "./assets/sander-dalhuisen-NFlyFizf2JU-unsplash.jpg";
-import usp3 from "./assets/aranxa-esteve-S5DEUg2yUVU-unsplash.jpg";
+import usp1 from "./assets/corporate-event.png";
+import usp2 from "./assets/party.png";
+import usp3 from "./assets/help.png";
 
 function App() {
   const handleDownloadEvent = (platform) => {
@@ -18,9 +17,7 @@ function App() {
     <div className="App">
       <nav>
         <div className="container">
-          <a href="/">
-            <img src={logoImage} alt="LOMAevents Logo" className="logo" />
-          </a>
+        <h1>LOMAevents</h1>
           <div className="links">
             <a href="https://LOMAeventshelpcenter.zendesk.com/hc/en-us/">
               Help Center
@@ -37,14 +34,12 @@ function App() {
         <div id="bg-LOMAevents"></div>
         <div className="container">
           <div className="hero-elements">
-            <h1>Download LOMAevents Today!</h1>
+          <div className="slogan">Crafting unforgettable moments!</div>
             <img
               className="LOMAevents-icon"
               src={logoImage}
               alt="LOMAevents Icon"
             />
-            <div className="slogan">Crafting unforgettable moments!</div>
-            <div className="app-links-qr-code">
               <div className="app-links">
                 <a
                   onClick={() => handleDownloadEvent("android")}
@@ -63,34 +58,30 @@ function App() {
                   <img src={appStoreImage} alt="App Store" />
                 </a>
               </div>
-              <div className="divider">
-                <div className="line"></div>
-                <div className="center">OR</div>
-                <div className="line"></div>
-              </div>
-              <div className="qr-container">
-                <img src={qrCodeImage} alt="QR Code" />
-                <div>Scan QR code to download LOMAevents on your device.</div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
 
       <div id="usps">
         <div className="container">
-          <h2>Why download LOMAevents?</h2>
+          <h3>Organize a Seamless Corporate Workshop</h3>
+          <br></br>
           <div className="usp">
             <img src={usp1} alt="Organize a Seamless Corporate Workshop" />
-            <h3>Organize a Seamless Corporate Workshop</h3>
+            <p>From venue selection to catering options, LOMAevents ensures your corporate workshop runs without a hitch. Our tailored recommendations are designed to match your team’s needs, providing real-time availability for smooth planning. Focus on your content, and let LOMAevents handle the logistics to deliver an impactful and professional experience.</p>
           </div>
+          <h3>Effortlessly Plan Your Perfect Event</h3>
+          <br></br>
           <div className="usp">
             <img src={usp2} alt="Effortlessly Plan Your Perfect Event" />
-            <h3>Effortlessly Plan Your Perfect Event</h3>
+            <p>LOMAevents takes the stress out of event planning by curating the best local vendors at your fingertips. Whether it’s an intimate gathering or a grand celebration, our app simplifies every step, from booking venues to coordinating catering. With personalized suggestions and real-time updates, your dream event is just a few taps away.</p>
           </div>
+          <h3>Plan a Fun Team Building Activity</h3>
+          <br></br>
           <div className="usp">
             <img src={usp3} alt="Plan a Fun Team Building Activity" />
-            <h3>Plan a Fun Team Building Activity</h3>
+            
+            <p>Bring your team together with engaging activities, handpicked to foster collaboration and creativity. LOMAevents connects you with the best local spots and services, providing instant recommendations tailored to your team-building goals. Let us streamline the process so you can focus on building stronger, more motivated teams.</p>
           </div>
         </div>
       </div>
@@ -105,6 +96,7 @@ function App() {
           <div className="copyright">
             Copyright © 2024 LOMAevents All Rights Reserved
           </div>
+          <a href="https://www.flaticon.com/free-icons/corporate" title="corporate icons">Icons created by Sir.Vector - Flaticon</a>
         </div>
       </footer>
     </div>
