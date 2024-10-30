1a4e10433be1ce2a73799894d0af215df5c14b9b
denfushi <awadomar@gmail.com>
2024-10-30 12:08:53 -0400

    nnpm fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 // src/index.js
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css"; // Import your global styles
-import App from "./App"; // Import the main App component
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"; // Make sure to import your CSS
 
-// Rendering the App component with BrowserRouter to handle routing
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
